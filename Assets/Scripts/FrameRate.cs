e3506728fbae92388465409ce8cfe59785a0e8d4
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2025-08-09 19:13:01 +0300

    updated project settings

--- Assets/Scripts/FrameRate.cs
+++ Assets/Scripts/FrameRate.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FramRate : MonoBehaviour
+public class FrameRate : MonoBehaviour
 {
     // Start is called before the first frame update
     private void Awake()
